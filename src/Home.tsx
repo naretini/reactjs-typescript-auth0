7d606ac7fe0d90c7cb6cb3bcf6fcf538ed4e8bd5
Nico Rtn <naretini@gmail.com>
2018-12-15 19:11:29 +0100

    Fix

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 
-import { Link } from 'react-router-dom'
 
 
 export default class Home extends React.Component<any, any>{
     public render() {
-        const { isAuthenticated, login } = this.props.auth;
         return (
             <div >
                 <h1>Home</h1>
